103e119b044b80f2da250332935721fe6e7d6f31
Mikko Lindholm <mlindhol@student.hive.fi>
2020-03-24 22:48:57 +0200

    added testing environment, fixed one faulty case

--- chall00/mlindhol.c
+++ chall00/mlindhol.c
@@ -20,7 +20,7 @@ int     hv_necklace(char *s1, char *s2)
 
     len2 = strlen(s2);
     i = 0;
-    j = -1;
+    j = 0;
 
     if (s1 == s2)
         return (1);
@@ -28,10 +28,12 @@ int     hv_necklace(char *s1, char *s2)
         return (0);
     if (!strchr(s2, s1[0]))
         return (0);
-    while (++j < len2)
+    while (j < len2)
     {
         while (s1[i] && s2[j] && s1[i] == s2[j])
         {
+            if (s1[i + 1] && s2[j + 1] && s1[i + 1] != s2[j + 1])
+                break;
             ++i;
             ++j;
             if (s1[i] && !s2[j])
@@ -44,6 +46,7 @@ int     hv_necklace(char *s1, char *s2)
                 return (1);
         }
         i = 0;
+        ++j;
     }
     return (0);
 }
