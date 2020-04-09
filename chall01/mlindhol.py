dc9b46aebf73d9eed93a2d89de096fac53e92b29
Mikko Lindholm <mlindhol@student.hive.fi>
2020-04-09 11:50:05 +0300

    new challanges and tests added

--- chall01/mlindhol.py
+++ chall01/mlindhol.py
@@ -1,29 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 
 
 def error():
-    print("usage: ./mlindhol.py <a-zA-Z string>")
+    print("usage: ./" + sys.argv[0] + "<a-zA-Z string>")
     exit(-1)
 
 
-if len(sys.argv) != 2 or len(sys.argv[1]) == 0:
-    error()
+def main():
 
-morse_array = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
-               "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
-
-input_str = sys.argv[1]
-
-for x in input_str:
-    av = ord(x)
-    if av < 32 or 32 < av < 65 or 90 < av < 97 or av > 122:
+    if len(sys.argv) != 2 or len(sys.argv[1]) == 0:
         error()
 
-for s in input_str:
-    if s is " ":
-        print(" ", end="")
-    else:
-        y = 64 if s.isupper() else 96
-        letter = ord(s) - y
-        print(morse_array[letter - 1], end="")
+    morse_array = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+                   "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+    input_str = sys.argv[1]
+
+    for x in input_str:
+        av = ord(x)
+        if av < 32 or 32 < av < 65 or 90 < av < 97 or av > 122:
+            error()
+
+    for s in input_str:
+        if s is " ":
+            print(" ", end="")
+        else:
+            y = 64 if s.isupper() else 96
+            letter = ord(s) - y
+            print(morse_array[letter - 1], end="")
+
+
+if __name__ == "__main__":
+    main()
